579744970264cff139eacefdf569ee725a1c988f
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-09-01 11:25:09 +0800

    added back start logic

--- server/main.ts
+++ server/main.ts
@@ -19,6 +19,12 @@ const handler = (req: Request): Response => {
         const channel = new BroadcastChannel('chat');
 
         const stream = new ReadableStream({
+            start: (controller) => {
+                channel.onmessage = (e) => {
+                    const body = `data: ${JSON.stringify(e.data)}\n\n`;
+                    controller.enqueue(body);
+                };
+            },
             cancel() {
                 channel.close();
             },
